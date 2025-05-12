74177a7d4acd9e5a59ddf9743f5bdb8214c06fe0
MartinFestingerVault <105220155+MartinFestingerVault@users.noreply.github.com>
2025-05-12 10:32:29 +0200

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -7,5 +7,5 @@ type SiteConfig = {
 export const siteConfig: SiteConfig = {
   site_name: "next-wp",
   site_description: "Starter template for Headless WordPress with Next.js",
-  site_domain: "https://next-wp.com",
+  site_domain: "http://j0c8ogwc8ggk0kkss08gw0k4.38.242.206.132.sslip.io",
 };
